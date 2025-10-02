cc13e0f0cc973ee2cdf32db542e2228e8b6bbdca
Khadija <memmedlixedice318@gmail.com>
2025-10-02 16:50:39 +0400

    data types&variables and Operators

---
+++ JavaScript/01.js-intro/Intro-operators/operator.js
@@ -0,0 +1,60 @@
+//  Operators
+
+//  =, +, -, *, 
+
+// 1. Aritmetic Operators
+// 2. Assigment Operators - = ve turleri
+// 3. Comparison Operators - > , <, >=, <=
+// 4. Logical Operators - mentiqi operatorlar ve, ve ya, inkar
+  
+
+// Arithmetic Operators
+let x;
+let y;
+x = 6;
+y = 5;
+console.log(x+y);
+console.log(x-y);
+console.log(x*y);
+console.log(x/y);
+console.log(x%y);
+
+// Assigment Operators
+let num1 = 15;
+let num2 = 3;
+
+console.log(num1+=num2); 
+console.log(num1-=num2); 
+console.log(num1*=num2); 
+console.log(num1/=num2); 
+console.log(num1%=num2);
+
+// Logical Operators 
+
+let a = true;
+let b = false;
+console.log(a && b);
+console.log(a || b);
+console.log(!a);
+
+let c = 5;
+let d = 10;
+console.log(c && d);
+console.log(c || d);
+console.log(!c);
+
+// Comparison Operators
+let e = 5;
+let f = 10;
+console.log(e > f);
+console.log(e < f);
+console.log(e >= f);
+console.log(e <= f);
+
+
+
+
+
+
+
+
