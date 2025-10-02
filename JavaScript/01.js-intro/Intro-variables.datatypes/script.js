850c62262681df4be2ecab7954799d6b0a75b5c9
Khadija <memmedlixedice318@gmail.com>
2025-10-02 16:57:39 +0400

    fix

---
+++ JavaScript/01.js-intro/Intro-variables.datatypes/script.js
@@ -0,0 +1,77 @@
+// console.log("Hello World");
+
+//  data types, variables, operators
+
+// Data Types 
+//  1. string - metn 
+//  2. number - reqem
+//  3. bollean - true/false
+//  4. null
+//  5. undefined
+
+
+// Variables
+//  1. let
+//  2. var
+//  3. const
+
+
+// initialization
+// decleration
+// assignment
+// reassignment
+// redeclaration
+
+// initialization
+// let x;
+// let x = 5;
+
+// decleration
+let x = 5;
+// const a = 5;  - decleration olmur, initialization olur, reassignment olmur, redeclaration olmur, block scope
+// let - hem decleration, hem initialization olur, reassignment olur, redeclaration olmur, assignment olur, block scope
+// var - decleration olur, initialization olur, reassignment olur, redeclaration olur, global scope
+
+// var student = "Daniz";
+// var student = "Khadija";
+// console.log(student);
+
+
+// assignment
+let student;
+student = "Deniz";
+// reassignment
+student = "Khadija";
+// console.log(student);
+
+
+// redecleration
+
+let userName = "Kanan";
+// let userName = "Hesen";
+console.log(userName);
+
+{
+  let x;
+  x = 12;
+  console.log(x);
+}
+
+{
+  const a = 34;
+  console.log(a);
+}
+
+// var b;
+{
+  // var b;
+  b= 56;
+  console.log(b);
+}
+ 
+// hoisting 
+// let userAge = 24;
+// alert(userAge);
+
+
+
